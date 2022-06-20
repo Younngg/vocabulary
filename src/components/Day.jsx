2d35286ba105e88d0ae8d8acb22c9be90d9a4509
Jiyeong Jeong <98656282+Younngg@users.noreply.github.com>
2022-06-21 00:11:44 +0900

    추가 : days, words fetch 추가

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import dummy from '../db/data.json';
 import { useParams } from 'react-router-dom';
 import Word from './Word';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Day = () => {
   const { day } = useParams();
-  const wordList = dummy.words.filter(
+  /* const wordList = dummy.words.filter(
     (word) => Number(word.day) === Number(day)
-  );
+  ); */
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then((res) => res.json())
+      .then((data) => setWords(data));
+  }, [day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import dummy from '../db/data.json';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const DayList = () => {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/days')
+      .then((res) => res.json())
+      .then((data) => setDays(data));
+  }, []);
+
   return (
     <ul className='list_day'>
-      {dummy.days.map((day) => (
+      {days.map((day) => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
