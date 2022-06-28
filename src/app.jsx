25f06816be3c92fbf366ca68ae5ff8f0d0d212cb
Jiyeong Jeong <98656282+Younngg@users.noreply.github.com>
2022-06-28 15:02:19 +0900

    추가 : 추가, 삭제 기능

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,8 @@ import DayList from './components/DayList';
 import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmptyPage from './components/EmptyPage';
+import CreateWord from './components/CreateWord';
+import CreateDay from './components/CreateDay';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route exact path='/' element={<DayList />} />
           <Route path='/day/:day' element={<Day />} />
           <Route path='*' element={<EmptyPage />} />
+          <Route path='/create_word' element={<CreateWord />} />
+          <Route path='/create_day' element={<CreateDay />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Word from './Word';
-import { useState } from 'react';
-import { useEffect } from 'react';
+
+import useFetch from '../hooks/useFetch';
 
 const Day = () => {
   const { day } = useParams();
-  /* const wordList = dummy.words.filter(
-    (word) => Number(word.day) === Number(day)
-  ); */
-  const [words, setWords] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:3001/words?day=${day}`)
-      .then((res) => res.json())
-      .then((data) => setWords(data));
-  }, [day]);
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
   return (
     <>
       <h2>Day {day}</h2>
+      {words.length === 0 && <span>Loading...</span>}
       <table>
         <tbody>
           {words.map((word) => (
